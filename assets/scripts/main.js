e055d9126a7f9d0e5ba6d9e3aef707e944276f28
yvntsu <yvonnnetsu@gmail.com>
2020-02-09 21:49:19 -0500

    fixed active based on scroll and smooth scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,11 +10,40 @@ $( document ).ready(function() {
         }
     });
 
-    // toggle active
+    // toggle active on click
     $(document).on('click', '.nav-items a', function() {
         $(".nav-items a").removeClass("active");
         $(this).addClass("active");
     });
+
+    $(document).ready(function() {
+		$('a[href*=#]').bind('click', function(e) {
+				e.preventDefault(); // prevent hard jump, the default behavior
+
+				var target = $(this).attr("href"); // Set the target as variable
+
+				// perform animated scrolling by getting top-position of target-element and set it as scroll target
+				$('html, body').stop().animate({
+						scrollTop: $(target).offset().top
+				}, 600, function() {
+						location.hash = target; //attach the hash (#jumptarget) to the pageurl
+				});
+
+				return false;
+		});
+    });
+
+    $(window).scroll(function() {
+            var scrollDistance = $(window).scrollTop();
+        
+            // Assign active class to nav links while scolling
+            $('.page-section').each(function(i) {
+                    if ($(this).position().top <= scrollDistance) {
+                            $('.nav-items a.active').removeClass('active');
+                            $('.nav-items a').eq(i).addClass('active');
+                    }
+            });
+    }).scroll();
 
     // changing colors based on mousemove
     // var $win = $(window),
